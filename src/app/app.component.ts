3baec46938732600818fc5064f79285cb2d2b0da
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-08-02 10:19:47 -0500

    se agrego el uso del componente de header en  la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Bienvenido a Angular';
-  course:string = 'Spring boot Api Rest'
+  course: string = 'Spring boot Api Rest';
 }
