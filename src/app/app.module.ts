fa21caec996bb3816a87f6353b442450d68e7109
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-08-03 09:31:01 -0500

    se creo y se  registro el componente directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DirectiveComponent } from './directive/directive.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, DirectiveComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
