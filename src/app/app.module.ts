f70e81cef0caf1edb5759ffdec93f7a6735bb40f
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-08-05 10:41:16 -0500

    se agregaron los import FormsModule y Httpclient a la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import { DirectiveComponent } from './directive/directive.component';
 import { ClientsComponent } from './clients/clients.component';
 import { ClientService } from './clients/client.service';
 import { Route, RouterModule } from '@angular/router';
+import { FormComponent } from './clients/form.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Route[] = [
   { path: '', redirectTo: '/clients', pathMatch: 'full' },
   { path: 'directives', component: DirectiveComponent },
   { path: 'clients', component: ClientsComponent },
+  { path: 'clients/create', component: FormComponent },
 ];
 
 @NgModule({
@@ -22,8 +26,14 @@ const routes: Route[] = [
     FooterComponent,
     DirectiveComponent,
     ClientsComponent,
+    FormComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule,
+  ],
   providers: [ClientService],
   bootstrap: [AppComponent],
 })
